50f9562e89b153ba20ac891f604e59dcdfea7789
nitishd22 <nitishd@gmail.com>
2022-07-10 20:47:13 -0700

    test

---
+++ nitish-portfolio/src/components/About/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Michael Giddings</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="https://avatars3.githubusercontent.com/u/11549316?s=460&u=9cbb3efedf0845c6fb20524c104ba92fff38c871&v=4" alt="Michael Giddings" />
+                <p>
+                Hi guys, my name is Nitish Doss, and I'm currently a college student studying Management Information Systems and minoring in Computer Science.
+
+                </p>
+                <p>
+                I'm familiar with many different programming languages and looking to work in the tech industry.
+                </p>
+                <p>
+                Feel free to browse my website and ask me any questions!
+                </p>
+            </div>
+        </section>
+    )
+}
